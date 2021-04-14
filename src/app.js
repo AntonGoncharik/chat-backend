eb833cb705403b28329c6872e29c6866fbf4b2af
AntonGoncharik <ant.goncharik@gmail.com>
2021-04-15 02:42:23 +0300

    added static folder

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(config.server.cors));
-// app.use('/avatars', express.static('avatars'));
+app.use('/avatars', express.static(`${process.env.PWD}/avatars`));
 app.use(usersMiddleware.checkUser);
 app.use('/', router);
 app.use(errorsMiddleware.sendError);
